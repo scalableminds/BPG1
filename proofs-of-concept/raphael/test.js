3088254cd1b8678454022b943eb4411857c1d96c
Anita Dieckhoff <anitadieckhoff@gmail.com>
2013-02-14 17:04:37 +0100

    group circles with texts and add random colors

--- proofs-of-concept/raphael/test.js
+++ proofs-of-concept/raphael/test.js
@@ -1,40 +1,50 @@
 $(function() {
 	var paper = Raphael('draw', 30, '100%', '100%');
+	
 	for (var i = 0; i < 20; ++i) {	
 		var random_w = Math.floor(Math.random()*500);
 		var random_h = Math.floor(Math.random()*500);
 		window['im_' + i] = "http://placehold.it/" + random_w + "x" + random_h;	
-	}	
+	}
 	
+//colors:
+	var col0 = 'purple';
+	var col1 = 'steelblue';
+	var col2 = 'forestgreen';
+	var col3 = 'orange';
+	var col4 = 'yellow';
+
+//images:	
 	paper.setStart();
-	for (var i = 0; i < 1000; ++i) {
+	for (var i = 0; i < 10; ++i) {
 		var random_x = Math.floor(Math.random()*1001);
 		var random_y = Math.floor(Math.random()*1001);
-		paper.image(eval('im_' + Math.floor(Math.random()*20)), random_x, random_y, 50, 50).id = i;
+		paper.image(eval('im_' + Math.floor(Math.random()*20)), random_x, random_y, 50, 50);
 	}
 	images = paper.setFinish();
-	
-	paper.setStart();
+
+//circles with text:
+	circles = paper.set();
 	for (var i = 0; i < 10000; ++i) {
+		var circle_and_text = paper.set();
+		
 		var random_x = Math.floor(Math.random()*2001);
 		var random_y = Math.floor(Math.random()*2001);
+		
 		var c = paper.circle(random_x, random_y, 30);
+		var t = paper.text(random_x, random_y, "test");
+		
 		c.attr({
-			fill: 'steelblue',
+			fill: eval('col' + Math.floor(Math.random()*5)),
 		});
-		c.id = i + 10000
+		
+		c.id = i;
+		
+		circle_and_text.push(c);
+		circle_and_text.push(t);
+		circles.push(circle_and_text);
 	}
-	circles = paper.setFinish();
-	
-	paper.setStart();
-	for (var i = 0; i < 10000; ++i) {
-		var c = paper.getById(i + 10000);
-		var x = c.attr("cx");
-		var y = c.attr("cy");
-		paper.text(x, y, "blabla").id = i + 2000;
-	}
-	texts = paper.setFinish();
-	
+		
 	var start = function () {
 		this.ox = this.attr("x");
 		this.oy = this.attr("y");
@@ -53,9 +63,8 @@ $(function() {
 		this.animate({opacity: .5}, 500, ">");
 	},
 	move_c = function (dx, dy) {
-		this.attr({cx: this.ox + dx, cy: this.oy + dy});
-		var t = paper.getById(this.id - 8000);
-		t.attr({x: this.ox + dx, y: this.oy + dy});
+		this.attr({cx: this.ox + dx, cy: this.oy + dy});		
+		circles[this.id][1].attr({x:this.ox + dx, y: this.oy + dy});
 	},
 	up_c = function () {
 		this.animate({opacity: 1.}, 500, ">");
