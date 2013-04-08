3cfd658641b5fef81d60a26e54d787e900c2af38
dominic-braeunlein <dominic.braeunlein@googlemail.com>
2013-04-08 15:55:14 +0200

    first thumbnail plugin started

--- project-zoom/app/Global.scala
+++ project-zoom/app/Global.scala
@@ -4,6 +4,7 @@ import projectZoom.connector.dropbox.DropboxConnector
 import projectZoom.core.artifact.ArtifactActor
 import projectZoom.core.knowledge.KnowledgeActor
 import projectZoom.core.event.EventActor
+import projectZoom.thumbnails.TextThumbnailActor
 
 object Global extends GlobalSettings {
 
@@ -12,6 +13,7 @@ object Global extends GlobalSettings {
     EventActor.start
     ArtifactActor.start    
     KnowledgeActor.start
+    TextThumbnailActor.start
     //DropboxConnector.startAggregating(app)
   }
 
---
+++ project-zoom/app/projectZoom/thumbnails/TextThumbnailJavaPlugin.java
@@ -0,0 +1,21 @@
+package projectZoom.thumbnails;
+
+public class TextThumbnailJavaPlugin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void onResourceFound() {
+		System.out.print("Hesdfllo ");
+		
+		
+	}
+	
+	
+
+}
