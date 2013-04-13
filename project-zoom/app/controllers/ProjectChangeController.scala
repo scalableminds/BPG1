6201940c7c076b02d47e71c66a6baec2a157b519
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-04-13 11:26:39 +0200

    work in progress

--- project-zoom/app/controllers/ProjectChangeController.scala
+++ project-zoom/app/controllers/ProjectChangeController.scala
@@ -21,13 +21,30 @@ import play.api.libs.json.JsObject
 import play.api.libs.json.JsString
 import play.api.libs.iteratee.Concurrent
 import play.api.libs.json.JsArray
+import securesocial.core.SecureSocial
+import scala.concurrent.Future
+import securesocial.core.UserId
 
-case class Join(username: String)
-case class Quit(username: String)
+case class Join(userId: UserId)
+case class Quit(userId: UserId)
 case class Connected(enumerator: Enumerator[JsValue])
 case class CannotConnect(msg: String)
 
-object ProjectRoom {
+trait ClosedChannelHelper {
+  def closedChannel(error: String) = {
+    // Connection error
+
+    // A finished Iteratee sending EOF
+    val iteratee = Done[JsValue, Unit]((), Input.EOF)
+
+    // Send an error and close the socket
+    val enumerator = Enumerator[JsValue](JsObject(Seq("error" -> JsString(error)))).andThen(Enumerator.enumInput(Input.EOF))
+
+    (iteratee, enumerator)
+  }
+}
+
+object ProjectRoom extends ClosedChannelHelper {
   implicit val sys = Akka.system(Play.current)
   implicit val timeout = Timeout(1 second)
 
@@ -36,13 +53,13 @@ object ProjectRoom {
   def createRoom(_project: String) =
     sys.actorOf(Props(new ProjectRoom(_project)))
 
-  def join(username: String, _project: String): scala.concurrent.Future[(Iteratee[JsValue, _], Enumerator[JsValue])] = {
+  def join(userId: UserId, _project: String): scala.concurrent.Future[(Iteratee[JsValue, _], Enumerator[JsValue])] = {
     val room = projectRooms().get(_project).getOrElse {
       val r = createRoom(_project)
       projectRooms.send(_ + (_project -> r))
       r
     }
-    (room ? Join(username)).map {
+    (room ? Join(userId)).map {
 
       case Connected(enumerator) =>
 
@@ -50,66 +67,59 @@ object ProjectRoom {
         val iteratee = Iteratee.foreach[JsValue] { event =>
           Logger.debug("Client sent Message: " + event)
         }.mapDone { _ =>
-          room ! Quit(username)
+          room ! Quit(userId)
         }
 
         (iteratee, enumerator)
 
       case CannotConnect(error) =>
 
-        // Connection error
-
-        // A finished Iteratee sending EOF
-        val iteratee = Done[JsValue, Unit]((), Input.EOF)
-
-        // Send an error and close the socket
-        val enumerator = Enumerator[JsValue](JsObject(Seq("error" -> JsString(error)))).andThen(Enumerator.enumInput(Input.EOF))
-
-        (iteratee, enumerator)
-
+        closedChannel(error)
     }
 
   }
 }
 
 class ProjectRoom(_project: String) extends Actor {
-  
-  var members = Set.empty[String]
+
+  var members = Set.empty[UserId]
   val (chatEnumerator, chatChannel) = Concurrent.broadcast[JsValue]
 
   def receive = {
-    
-    case Join(username) => {
-      if(members.contains(username)) {
+
+    case Join(userId) => {
+      if (members.contains(userId)) {
         sender ! CannotConnect("This user already joined.")
       } else {
-        Logger.debug(s"ProjectRoom.Join $username has joined the room")
-        members = members + username
+        Logger.debug(s"ProjectRoom.Join ${userId.id} has joined the room")
+        members = members + userId
         sender ! Connected(chatEnumerator)
       }
     }
 
-    case Quit(username) => {
-      members = members - username
-      Logger.debug(s"ProjectRoom.Quit $username has left the room")
+    case Quit(userId) => {
+      members = members - userId
+      Logger.debug(s"ProjectRoom.Quit {userId.id} has left the room")
     }
   }
-  
+
   def notifyAll(kind: String, user: String, text: String) {
     val msg = JsObject(
       Seq(
         "kind" -> JsString(kind),
         "user" -> JsString(user),
-        "message" -> JsString(text)
-      )
-    )
+        "message" -> JsString(text)))
     chatChannel.push(msg)
   }
 }
 
-object ProjectChangeController extends ControllerBase {
+object ProjectChangeController extends ControllerBase with SecureSocial with ClosedChannelHelper {
 
-  def joinChannel(_project: String) = WebSocket.async[JsValue] { request =>
-     ProjectRoom.join("username", _project)
+  def joinChannel(_project: String) = WebSocket.async[JsValue] { implicit request =>
+    SecureSocial.currentUser(request).map { user =>
+      ProjectRoom.join(user.id, _project)
+    } getOrElse {
+      Future.successful(closedChannel("Not logged in."))
+    }
   }
 }
--- project-zoom/app/dominic.sc
+++ project-zoom/app/dominic.sc
@@ -1,12 +1,12 @@
 object dominic {
-	import projectZoom.thumbnails.text.TextThumbnailPlugin
-	import akka.actor._
-	
-	/*
-	val system = ActorSystem("mysystem")
-	val thumbnailboy = system.actorOf(Props[ThumbnailActor], "thumbnailActor")
-	thumbnailboy ! 5
-	
+  import projectZoom.thumbnails.text.TextThumbnailPlugin
+  import akka.actor._
+  
+  /*
+  val system = ActorSystem("mysystem")
+  val thumbnailboy = system.actorOf(Props[ThumbnailActor], "thumbnailActor")
+  thumbnailboy ! 5
+  
   println("Welcome to the Scala worksheet")
   */
   
@@ -14,5 +14,5 @@ object dominic {
   plugin.onResourceFound()
   //plugin.onResourceFound();
   
-  
+   println("test")
 }
