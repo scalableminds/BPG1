140cd0beb418d66151490dde5528fe6b299ac39a
Thomas Werkmeister <thomas@scm.io>
2013-05-14 15:19:30 +0200

    moved oauth to extra controller

--- project-zoom/app/controllers/Admin.scala
+++ project-zoom/app/controllers/Admin.scala
@@ -11,17 +11,5 @@ object Admin extends ControllerBase with SecureSocial with PlayActorSystem with 
   def panel = SecuredAction { implicit request => 
     Ok(views.html.adminPanel())
   }
-  
-  def redirectToBoxAuth = SecuredAction { implicit request =>
-    (for{client_id <- config.getString("box.client_id")
-    } yield {
-      Redirect(s"https://www.box.com/api/oauth2/authorize?response_type=code&client_id=$client_id&state=authenticated")
-    }) getOrElse Ok("unfortunately box is not configured")
-  }
-  
-  def boxAuthenticated(code: String, state: String) = SecuredAction { implicit request =>
-    Logger.info(s"code: $code")
-    Logger.info(s"state: $state")
-    Ok(views.html.adminPanel())
-  }
+ 
 }
