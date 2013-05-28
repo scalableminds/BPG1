d574d13de08e26a36a083aad8db1b94832ad1e2f
dominic-braeunlein <dominic.braeunlein@googlemail.com>
2013-05-28 18:34:21 +0200

    alt icon folder added

--- project-zoom/modules/common/app/projectZoom/thumbnails/IconPlugin.java
+++ project-zoom/modules/common/app/projectZoom/thumbnails/IconPlugin.java
@@ -13,7 +13,8 @@ import models.ResourceInfo;
 public class IconPlugin {
 	
 	static String ICON_FOLDER = "public/icons/";
-	static String defaultIcon = "unknown";
+	static String ALT_ICON_FOLDER = "modules/common/public/icons/";
+	static String DEFAULT_ICON = "unknown";
 
 	
 	public IconPlugin() {
@@ -22,26 +23,30 @@ public class IconPlugin {
 	
 	public InputStream onResourceFound(File resource, ResourceInfo ressourceInfo) {
 		
-		System.out.print("onResourceFound called ");
-		System.out.print(System.getProperty("user.dir"));
+		System.out.println("Icon onResourceFound called ");
 		String fn = ressourceInfo.name();
 		String ext = fn.substring(fn.lastIndexOf(".") + 1);
 
 		String url = ICON_FOLDER + ext + ".png";
-		System.out.print(ext);
+		System.out.println(ext);
 	
-		if (url!=null) {
-			try {
-				return new FileInputStream(url);
-			} catch (FileNotFoundException e) {
-				//e.printStackTrace();
-			}
-		}
-		
-		String urlDefault = ICON_FOLDER + defaultIcon + ".png";
-
 		try {
-			return new FileInputStream(urlDefault.toString());
+			return new FileInputStream(url);
+		} catch (FileNotFoundException e) {}
+
+		String altUrl = ICON_FOLDER + ext + ".png";
+		try {
+			return new FileInputStream(altUrl);
+		} catch (FileNotFoundException e) {}
+
+		String urlDefault = ICON_FOLDER + DEFAULT_ICON + ".png";
+		try {
+			return new FileInputStream(urlDefault);
+		} catch (FileNotFoundException e) {}
+		
+		String altUrlDefault = ALT_ICON_FOLDER + DEFAULT_ICON + ".png";
+		try {
+			return new FileInputStream(altUrlDefault);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return new ByteArrayInputStream(new byte[0]);
