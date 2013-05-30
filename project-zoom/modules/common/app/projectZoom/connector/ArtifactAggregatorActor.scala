9d75357875809b1638fed7d1b4c9e38f8effe763
Thomas Werkmeister <thomas@scm.io>
2013-05-30 13:48:57 +0200

    added correct artifact events to artifactaggregator

--- project-zoom/modules/common/app/projectZoom/connector/ArtifactAggregatorActor.scala
+++ project-zoom/modules/common/app/projectZoom/connector/ArtifactAggregatorActor.scala
@@ -3,23 +3,23 @@ package projectZoom.connector
 import java.io.File
 import models.ArtifactLike
 import projectZoom.core.event.{EventPublisher, Event}
-
-case class ArtifactFound(file: File, artifact: ArtifactLike) extends Event
-case class ArtifactDeleted(artifact: ArtifactLike) extends Event
-case class ArtifactAggregation(l: List[ArtifactFound]) extends Event
+import projectZoom.core.artifact._
+import java.io.ByteArrayInputStream
 
 trait ArtifactAggregatorActor extends ConnectorActor with EventPublisher {
 
-  def publishFoundArtifact(file: File, artifact: ArtifactLike) = {
-    publish(ArtifactFound(file, artifact))
+  def toInputStream(bytes: Array[Byte]) = new ByteArrayInputStream(bytes)
+  
+  def publishFoundArtifact(bytes: Array[Byte], artifact: ArtifactLike) = {
+    publish(ArtifactFound(toInputStream(bytes), artifact))
   }
 
   def publishDeletedArtifact(artifact: ArtifactLike) = {
     publish(ArtifactDeleted(artifact))
   }
 
-  def publishAggregatedArtifacts(l: List[Tuple2[File, ArtifactLike]]) = {
-    val foundArtifacts = for (pair <- l) yield ArtifactFound(pair._1, pair._2)
-    publish(ArtifactAggregation(foundArtifacts))
+  def publishAggregatedArtifacts(l: List[Tuple2[Array[Byte], ArtifactLike]]) = {
+    val foundArtifacts = for (pair <- l) yield ArtifactFound(toInputStream(pair._1), pair._2)
+    publish(ArtifactAggregation("",foundArtifacts))
   }
 }
