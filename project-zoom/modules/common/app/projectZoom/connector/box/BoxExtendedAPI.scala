5d086ffa554a22874f2286e469e8ca71bb2dd525
Thomas Werkmeister <thomas@scm.io>
2013-06-03 13:41:34 +0200

    trying to reduce number of requests

--- project-zoom/modules/common/app/projectZoom/connector/box/BoxExtendedAPI.scala
+++ project-zoom/modules/common/app/projectZoom/connector/box/BoxExtendedAPI.scala
@@ -7,10 +7,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.libs.json._
 import play.api.Logger
 
-class BoxExtendedAPI(appKeys: BoxAppKeyPair) extends BoxAPI(appKeys) {
+import akka.agent._
+import projectZoom.util.PlayActorSystem
+import akka.util.Timeout
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class BoxExtendedAPI(appKeys: BoxAppKeyPair) extends BoxAPI(appKeys) with PlayActorSystem {
   type FolderId=String
   
-  val collaborations = scala.collection.mutable.Map[FolderId, List[BoxMiniUser]]()
+  implicit val timeout = Timeout(5 seconds)
+  val collaborations = Agent[Map[FolderId, List[BoxMiniUser]]](Map())
   
   def enumerateEvents(streamPos: Long = 0)(implicit accessTokens: BoxAccessTokens) = {
     def loop(stream_position: Long, accumulatedEvents: JsArray): Future[(Long, Future[JsArray])] = {
@@ -35,12 +44,12 @@ class BoxExtendedAPI(appKeys: BoxAppKeyPair) extends BoxAPI(appKeys) {
     fetchCollaboratorsList(folder)
     
   private def fetchCollaboratorsList(folder: BoxBaseFolder)(implicit accessTokens: BoxAccessTokens): Future[List[BoxMiniUser]] = {
-    Future(collaborations(folder.id)) fallbackTo
+    collaborations.future.map{map => map(folder.id)} fallbackTo
     fetchFolderCollaborations(folder.id).map{ json =>
       (json \ "entries").validate[List[BoxCollaboration]] match {
         case JsSuccess(collaborationList, _) => 
           val collaborators = collaborationList.flatMap(c => c.accessible_by)
-          collaborations += (folder.id -> collaborators)
+          collaborations.send(_ + (folder.id -> collaborators))
           collaborators
         case JsError(err) => Logger.error(s"Error fetching collaborations:\n ${err.mkString}\njson:\n${Json.stringify(json)}")
         List()
