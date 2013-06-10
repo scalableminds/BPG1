886a8118f87716947d6a6572b22af3241d9661f1
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-06-10 13:31:38 +0200

    removed comment

--- project-zoom/modules/common/app/projectZoom/core/artifact/ArtifactActor.scala
+++ project-zoom/modules/common/app/projectZoom/core/artifact/ArtifactActor.scala
@@ -103,7 +103,6 @@ class ArtifactActor extends EventSubscriber with EventPublisher with FSWriter wi
             ArtifactDAO.insertResource(artifact)(hash, resource).map( updated =>
               publish(ResourceInserted(file, updated getOrElse artifact, resource)))
           case Some(r) if r.hash != hash =>
-            // TODO: remove comment
             ArtifactDAO.updateHashOfResource(artifact)(hash, resource).map(updated =>
               publish(ResourceUpdated(file, updated getOrElse artifact, resource)))
           case _ =>
